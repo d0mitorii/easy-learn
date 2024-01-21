fa3c80fcae0fdb1c034cb539f1617109ec4bb724
machinescream <machinescream@icloud.com>
2024-01-21 15:46:06 +0100

    feat: logo

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native'; // Import Image
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.gg to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={require('./assets/logo.png')} // Use require for local images
+        style={styles.logo} // Apply the style
+      />
     </View>
   );
 }
@@ -17,4 +19,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 300, // Set width to 100
+    height: 300, // Set height to 100
+    backgroundColor: "transparent",
+    resizeMode: 'contain' // This ensures the aspect ratio is maintained
+  }
 });
